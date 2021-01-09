8834bfa2500c1c233b58c48741b4d1971da8ee2e
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2021-01-10 00:55:42 +0530

    Redux Storage

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -1,30 +1,28 @@
 import actionConstant from './actionTypes'
 import axios from 'axios'
 
+// The Api calls for displaying all country list in dropdown
 export const getCountryRequest = () => ({
     type: actionConstant.COUNTRIES_DATA_REQUEST
 })
-
 export const getCountrySuccess = (payload) => ({
     type: actionConstant.COUNTRIES_DATA_SUCCESS,
     payload
 })
-
 export const getCountryFailure = () => ({
     type: actionConstant.COUNTRIES_DATA_FAILURE
 })
-
 export const getCountryApiCall = (payload) => (dispatch) => {
     dispatch(getCountryRequest())
     axios.get(`https://disease.sh/v3/covid-19/countries`)
 
     .then((res)=>{
-        
+        // console.log(res.data)
         dispatch(getCountrySuccess(res.data))
     })
     .catch((err) => dispatch(getCountryFailure()))
 }
-
+// The Api calls to get the total number of cases, deaths and recovered
 export const countryAllDataRequest = () => ({
     type : actionConstant.GET_ALL_COUNTRIES_DATA_REQUEST
 })
@@ -39,31 +37,51 @@ export const getCountryAllData = (  ) => (dispatch) => {
     dispatch(countryAllDataRequest())
     axios.get(`https://disease.sh/v3/covid-19/all`)
     .then((res) => {
-        console.log(res.data)
-        dispatch(countryAllDataSuccess())
+        // console.log(res.data)
+        dispatch(countryAllDataSuccess(res.data))
     })
     .catch((err) => {
         dispatch(countryAllDataFailure())
     })
 
 }
-
+// The Api calls to get the total number of cases , deaths and recovered for a specific country
 export const countrySpecificDataRequest = () => ({
     type : actionConstant.GET_SPECIFIC_COUNTRIES_DATA_REQUEST
 })
-export const countrySpecificDataSuccess = () => ({
-    type : actionConstant.GET_SPECIFIC_COUNTRIES_DATA_SUCCESS
+export const countrySpecificDataSuccess = (payload) => ({
+    type : actionConstant.GET_SPECIFIC_COUNTRIES_DATA_SUCCESS,
+    payload
 })
 export const countrySpecificDataFailure = () => ({
     type : actionConstant.GET_SPECIFIC_COUNTRIES_DATA_FAILURE
 })
 export const getCountryData = ( payload ) => (dispatch) => {
-    console.log(payload)
     dispatch(countrySpecificDataRequest())
     axios.get(`https://disease.sh/v3/covid-19/countries/${payload}`)
     .then((res) => {
-        // console.log(res.data)
-        dispatch(countrySpecificDataSuccess())
+        dispatch(countrySpecificDataSuccess(res.data))
     })
     .catch(err => dispatch(countrySpecificDataFailure()))
+}
+// The Api calls for the history of last 30 days
+export const historicalDataRequest = () => ({
+    type : actionConstant.HISTORICAL_DATA_REQUEST
+})
+export const historicalDataSuccess = (payload) => ({
+    type : actionConstant.HISTORICAL_DATA_SUCCESS,
+    payload
+})
+export const historicalDataFailure = () => ({
+    type : actionConstant.HISTORICAL_DATA_FAILURE
+})
+export const getHistoricalData = () => (dispatch) => {
+    dispatch(historicalDataRequest())
+    axios.get(`https://disease.sh/v3/covid-19/historical/all/?lastdays=120`)
+    .then(res => {
+        dispatch(historicalDataSuccess(res.data))
+    })
+    .catch(err => {
+        dispatch(historicalDataFailure())
+    })
 }
--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -2,30 +2,40 @@ import actionConstant from './actionTypes'
 
 export const initState = {
     countriesArray :  [],
-    allCountriesData  : []
+    allCountriesData  : [],
+    specificData : [],
+    pastData : []
 }
 
 const reducer = ( state = initState , action ) => {
     switch(action.type){
+    //Reducer for displaying all country list in dropdown
         case actionConstant.COUNTRIES_DATA_REQUEST:
             return {
                 ...state ,
             }
-
         case actionConstant.COUNTRIES_DATA_SUCCESS:
+            // console.log(action.payload)
             return {
                 ...state,
                 countriesArray : action.payload.map((res) => ({
                     name : res.country,
-                    value : res.countryInfo.iso2
+                    value : res.countryInfo.iso2,
+                    cases : res.cases,
+                    lat : res.countryInfo.lat,
+                    long : res.countryInfo.long,
+                    flag : res.countryInfo.flag,
+                    recovered : res.recovered, 
+                    deaths : res.deaths
+
                 })),
                
             }
-
         case actionConstant.COUNTRIES_DATA_FAILURE:
             return{
                 ...state
             }
+    //Reducer to get the total number of cases, deaths and recovered
         case actionConstant.GET_ALL_COUNTRIES_DATA_REQUEST:
             return {
                 ...state
@@ -39,19 +49,36 @@ const reducer = ( state = initState , action ) => {
             return {
                 ...state
             }
+    //Reducer to get the total number of cases , deaths and recovered for a specific country  
         case actionConstant.GET_SPECIFIC_COUNTRIES_DATA_REQUEST:
             return {
-                ...state
+                ...state,
+               
             }
         case actionConstant.GET_SPECIFIC_COUNTRIES_DATA_SUCCESS :
             return{
-                ...state
+                ...state,
+                specificData : action.payload
             }
         case actionConstant.GET_SPECIFIC_COUNTRIES_DATA_FAILURE:
             return {
                 ...state
             }       
-
+    //Reducer for history of last 30 days
+        case actionConstant.HISTORICAL_DATA_REQUEST:
+            return {
+                ...state
+            }
+        case actionConstant.HISTORICAL_DATA_SUCCESS:
+            // console.log(action.payload)
+            return {
+                ...state,
+                pastData : action.payload
+            }
+        case actionConstant.HISTORICAL_DATA_FAILURE:
+            return {
+                ...state
+            }
         default :
             return state
     }
