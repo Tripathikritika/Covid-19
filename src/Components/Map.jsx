7108af86d3588b8462996c0266b6947b7650b855
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2021-01-10 00:49:52 +0530

    Map Functionality

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { MapContainer, TileLayer } from "react-leaflet";
+import styles  from "../Styling/Map.module.css"
+import { showDataOnMap } from './util';
 
-function Map() {
+function Map({countries,casesType,center ,zoom}) {
+    // console.log(countries)
     return (
-        <div className="map">
-            <h1>I m a map</h1>
-            
+        <div className={styles.map}>
+            <MapContainer center={center} zoom={zoom} className={styles.leafletContainer}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {showDataOnMap(countries,casesType)}
+            </MapContainer>
         </div>
     )
 }
-
 export default Map
